653a32e6ec70e933231f8fe38f9cfa4d11607bfb
Arthur Pitzer <arthurpitzer@id.uff.br>
2017-12-16 18:12:49 -0200

    trying portuguese tagger

--- interpreter.py
+++ interpreter.py
@@ -1,14 +1,18 @@
+import pickle
 import nltk
 from nltk import tokenize
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
+tagger = pickle.load(open("tagger.pkl"))
+portuguese_sent_tokenizer = nltk.data.load("tokenizers/punkt/portuguese.pickle")
+
 def interpret(sender, message):
-    tokens = tokenize.word_tokenize(message, language='portuguese')
-    tagged_words = nltk.pos_tag(tokens)
+    sentences = portuguese_sent_tokenizer.tokenize(text)
+    tagged_words = [tagger.tag(nltk.word_tokenize(sentence)) for sentence in sentences]
     answer = []
     for word, tag in tagged_words:
         if (tag.startswith('NN')):
             answer.append(word)
-    return ' ou '.join(answer) + '?'
+    return ' '.join(answer) + '?'
