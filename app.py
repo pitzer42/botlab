8b5cc05a1395a5dfe3cbfe5d95be331e671f3456
Arthur Pitzer <arthurpitzer@id.uff.br>
2017-12-16 11:13:41 -0200

    run loca script. bug correction on app.py

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ import requests
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "ok ok", 200
-
 @app.route('/', methods=['GET'])
 def verify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    return "ok ok", 200
+    return "Hello world", 200
 
 if __name__ == "__main__":
     app.run()
