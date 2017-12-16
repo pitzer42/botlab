1c8c7d996ba9bab8456128c06ed953fd18403629
Arthur Pitzer <arthurpitzer@id.uff.br>
2017-12-16 18:04:32 -0200

    identify nouns

--- interpreter.py
+++ interpreter.py
@@ -2,7 +2,13 @@ import nltk
 from nltk import tokenize
 
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 def interpret(sender, message):
     tokens = tokenize.word_tokenize(message, language='portuguese')
-    return tokens[0]
+    tagged_words = nltk.pos_tag(tokens)
+    answer = []
+    for word, tag in tagged_words:
+        if (tag.startswith('N')):
+            answer.append(word)
+    return ' ou '.join(answer) + '?'
