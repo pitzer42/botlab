6e828c29557cc85f5aefa03cc7d33e49f9af225a
Arthur Pitzer <arthurpitzer@id.uff.br>
2017-12-16 22:47:33 -0200

    quick deploy

--- interpreter.py
+++ interpreter.py
@@ -7,35 +7,20 @@ LANG = 'portuguese'
 LANG_CODE = 'pt-2'
 
 nltk.download('punkt')
-nltk.download('stopwords')
 nltk.download('rslp')
 
 def interpret(sender, message):
-    tokens = tokenize.word_tokenize(message)#, language=LANG)
-    #tokens = remove_stopwords(tokens)
+    tokens = tokenize.word_tokenize(message, language=LANG)
     tokens = tag(tokens)
     tokens = keep_tagged_tokens('NOUN', tokens)
     return ' ou '.join(tokens) + '?'
 
-def keep_tagged_tokens(tag, tagged_tokens):
-    result = []
-    for i in tagged_tokens:
-        if(i[1] == tag):
-            print(i)
-            result.append(i[0])
-    return result
-
 def tag(tokens):
-    #RSLPStemmer uses xrange from python 2.7 and was the best POS tagger found.
-    #this is a hack to replace all calls to xrange to range.
-    __builtins__['xrange'] = range
-    tagger = Tagger(language=LANG_CODE)
-    return tagger.tag(' '.join(tokens))
+	#RSLPStemmer uses xrange from python 2.7 and was the best POS tagger found.
+	#this is a hack to replace all calls to xrange to range.
+	__builtins__['xrange'] = range
+	tagger = Tagger(language=LANG_CODE)
+	return tagger.tag(' '.join(tokens))
 
-def stem(tokens):
-    stemmer = RSLPStemmer()
-    return stemmer.stem(tokens)
-
-def remove_stopwords(tokens):
-    stop_words = set(nltk.corpus.stopwords.words(LANG))
-    return [token for token in tokens if not token in stop_words]
+def keep_tagged_tokens(tag, tagged_tokens):
+	return [token for token, token_tag in tagged_tokens if token_tag == tag]
